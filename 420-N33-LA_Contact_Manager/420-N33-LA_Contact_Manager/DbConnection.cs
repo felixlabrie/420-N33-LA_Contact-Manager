76095a681e778ee22df713ad24d88830c671ada6
Tristan Giguere <tristangiguere@gmail.com>
2020-12-10 13:07:15 -0500

    Separate connection class

---
+++ 420-N33-LA_Contact_Manager/420-N33-LA_Contact_Manager/DbConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420_N33_LA_Contact_Manager
+{
+    class DbConnection
+    {
+        // Define all connection info
+        public static SqlConnection conn = null;
+            
+            public void ContactDb_Connection()
+        {
+            // Create connection - Connection string + parameters for queries.
+
+            // Define basic elements needed so that we can make an object for each connection
+
+            // Define variables to modify query (switch statement?)...
+        }
+    }
+}
